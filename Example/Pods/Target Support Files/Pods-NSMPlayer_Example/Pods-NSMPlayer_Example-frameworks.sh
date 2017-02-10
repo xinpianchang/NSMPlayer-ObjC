c02543cf340ce7222522f7a4bee32134e8da5393
linlin <linlin@vmovier.com>
2017-02-10 15:14:04 +0800

    Add Bolts

--- Example/Pods/Target Support Files/Pods-NSMPlayer_Example/Pods-NSMPlayer_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-NSMPlayer_Example/Pods-NSMPlayer_Example-frameworks.sh
@@ -89,9 +89,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NSMPlayer/NSMPlayer.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NSMPlayer/NSMPlayer.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then

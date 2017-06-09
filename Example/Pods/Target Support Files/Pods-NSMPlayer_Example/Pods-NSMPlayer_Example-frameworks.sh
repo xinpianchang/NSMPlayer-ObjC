c76f39517a6379b22cb9e3abf429817b40a8eaa6
Chengqihan <chengqihan@vmovier.com>
2017-06-09 11:18:47 +0800

    Update pod files

--- Example/Pods/Target Support Files/Pods-NSMPlayer_Example/Pods-NSMPlayer_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-NSMPlayer_Example/Pods-NSMPlayer_Example-frameworks.sh
@@ -91,7 +91,6 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NSMPlayer/NSMPlayer.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NSMStateMachine/NSMStateMachine.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Reachability/Reachability.framework"
@@ -99,7 +98,6 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NSMPlayer/NSMPlayer.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NSMStateMachine/NSMStateMachine.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Reachability/Reachability.framework"
